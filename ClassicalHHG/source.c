07ca98ecbc05e5ef5048afdd9f1e29016948408a
diogenmishel@gmail.com <diogenmishel@gmail.com>
2023-04-11 02:01:02 +0300

    Отладка работы алгоритма

--- ClassicalHHG/source.c
+++ ClassicalHHG/source.c
@@ -18,8 +18,8 @@ const double Ip = 21.55;  // потенциал ионизации (Ne) эВ
 const double Wxuv = 30;   // частота XUV, эВ
 const double Wir = 1;     // частота IR, эВ
 const double Txuv = 0.55; // время XUV, фс
-const double Tir = 20;    // время IR, фс
-const double fi = 0 * M_PI / 2; // относительная фаза огибающей
+const double Tir = 7 * M_PI;    // время IR, фс
+const double fi = 0 * M_PI_2; // относительная фаза огибающей
 #pragma endregion
 
 #pragma region выбор случая (монохромат, импульсы, IR, IR + XUV)
@@ -42,8 +42,7 @@ int nextIndex = 0;
 #pragma region параметры системы уравнений
 struct parameters
 {
-	double E; // энергия HHG
-	double k; // параметр Келдыша (модифицированный)
+	double E; // энергия HHG (или электрона, зависит от записи)
 	double C; // константа
 };
 #pragma endregion
@@ -104,7 +103,7 @@ double deltaE(double t1, double t2, double C)
 {
 	double sqrK = 2 * Ip;
 	double a = sqrK / (t2 - t1);
-	return a * P(t2, t1, t2, C) / (Wir * F(t1));
+	return a * P(t2, t1, t2, C) / (2 * Wir * F(t1));
 }
 #pragma endregion
 
@@ -138,22 +137,21 @@ int HHG_f(const gsl_vector * x, void * params, gsl_vector * func)
 {
 	struct parameters * p = (struct parameters *)params;
 	const double E = (p->E);
-	const double k = (p->k);
 	const double C = (p->C);
 
 	const double t1 = gsl_vector_get(x, 0);
 	const double t2 = gsl_vector_get(x, 1);
 
 	double e1, e2;
-	if (k == 0)
+	if (xuv_ir == 1)
 	{
 		e1 = P(t1, t1, t2, C);
-		e2 = (2 * Up * pow(P(t2, t1, t2, C), 2)) - (E/* - Ip*/)/* + (deltaE(t1, t2, C) / 2)*/;
+		e2 = (2 * Up * pow(P(t2, t1, t2, C), 2)) - (E);
 	}
 	else
 	{
 		e1 = (2 * Up * pow(P(t1, t1, t2, C), 2)) - (Wxuv - Ip);
-		e2 = (2 * Up * pow(P(t2, t1, t2, C), 2)) - (E/* - Ip*/);
+		e2 = (2 * Up * pow(P(t2, t1, t2, C), 2)) - (E);
 	}
 	gsl_vector_set(func, 0, e1);
 	gsl_vector_set(func, 1, e2);
@@ -163,7 +161,7 @@ int HHG_f(const gsl_vector * x, void * params, gsl_vector * func)
 #pragma endregion
 
 #pragma region поиск максимумов
-int max_t(double to1, double max_t1[N][2], double max_t2[N][2], double max_hhg[N], double C)
+int max_t(double to1, double max_t1[N][2], double max_t2[N][2], double max_e[N], double C)
 {
 	if (to1 > Tir)
 		return 0;
@@ -176,7 +174,7 @@ int max_t(double to1, double max_t1[N][2], double max_t2[N][2], double max_hhg[N
 	size_t iter = 0;
 
 	const size_t n = 2;
-	struct parameters p = { 0, 0, C };
+	struct parameters p = { 0, C };
 
 	gsl_multiroot_function func =
 	{
@@ -191,7 +189,7 @@ int max_t(double to1, double max_t1[N][2], double max_t2[N][2], double max_hhg[N
 
 	for (int i = 0; i < N; i++)
 	{
-		double to2 = to1 + ((2 + i) * M_PI) - (M_PI / 2);
+		double to2 = to1 + ((2 + i) * M_PI) - M_PI_2;
 		if (to2 > Tir)
 			break;
 
@@ -216,11 +214,14 @@ int max_t(double to1, double max_t1[N][2], double max_t2[N][2], double max_hhg[N
 			if (t1 < 0 || t2 > Tir)
 				continue;
 
-			max_t1[i][0] = max_t1[i][1] = t1;
-			max_t2[i][0] = max_t2[i][1] = t2;
-
 			double hhg = maxHHG(t1, t2, C);
-			max_hhg[i] = hhg/* + Ip*/;
+			if (hhg < 5)
+				continue;
+
+			max_t1[count][0] = max_t1[count][1] = t1;
+			max_t2[count][0] = t2 - 0.3;
+			max_t2[count][1] = t2 + 0.3;
+			max_e[count] = hhg;
 
 			printf("t1 = %.3e \n", t1);
 			printf("t2 = %.3e \n", t2);
@@ -243,14 +244,14 @@ int max_t(double to1, double max_t1[N][2], double max_t2[N][2], double max_hhg[N
 #pragma endregion
 
 #pragma region построение траекторий
-int trajectories(double k, double C, double to1)
+int trajectories(double to1, double C)
 {
     #pragma region нахождение максимумов
 	double max_t1[N][2];
 	double max_t2[N][2];
-	double max_hhg[N];
+	double max_e[N];
 
-	const int n = max_t(to1, max_t1, max_t2, max_hhg, C);
+	const int n = max_t(to1, max_t1, max_t2, max_e, C);
 	if (n == 0)
 		return 0;
     #pragma endregion
@@ -275,19 +276,15 @@ int trajectories(double k, double C, double to1)
     #pragma region отрисовка траекторий
 	for (int i = 0; i < n; i++)
 	{
-		double maxE = max_hhg[i];
-		double dE = maxE / M;
+		array_t[nextIndex] = max_t2[i][0] + 0.3;
+		array_E[nextIndex] = max_e[i];
+		nextIndex++;
 
 		for (int j = 0; j < 2; j++)
 		{
-			int sign = (2 * j) - 1;
-			max_t2[i][j] += sign * 0.25;
-
-			for (int l = M; l > 0; l--)
+			for (int E = (int)max_e[i]; E > 0; E--)
 			{
-				double E = l * dE;
-
-				struct parameters p = { E, k, C };
+				struct parameters p = { E, C };
 				func.params = &p;
 
 				gsl_vector_set(x, 0, max_t1[i][j]);
@@ -307,21 +304,21 @@ int trajectories(double k, double C, double to1)
 				} 
 				while (status == GSL_CONTINUE && ++iter < 1000);
 
-				if (status == GSL_SUCCESS)
-				{
-					double t1 = gsl_vector_get(s->x, 0);
-					double t2 = gsl_vector_get(s->x, 1);
+				if (status != GSL_SUCCESS)
+					continue;
+				
+				double t1 = gsl_vector_get(s->x, 0);
+				double t2 = gsl_vector_get(s->x, 1);
 
-					max_t1[i][j] = t1;
-					max_t2[i][j] = t2;
+				if (abs(max_t2[i][j] - t2) > M_PI_4)
+					break;
 
-					if (nextIndex == D)
-						continue;
+				max_t1[i][j] = t1;
+				max_t2[i][j] = t2;
 
-					array_t[nextIndex] = t2;
-					array_E[nextIndex] = E;
-					nextIndex++;
-				}
+				array_t[nextIndex] = t2;
+				array_E[nextIndex] = E;
+				nextIndex++;
 			}
 		}
 	}
@@ -336,15 +333,40 @@ int trajectories(double k, double C, double to1)
 #pragma endregion
 
 #pragma region запись в файл
-void writeFILE()
+void writeFILE(double C)
 {
-	FILE * fp;
+    #pragma region траектории
+	FILE* fp;
 	fp = fopen("HHG.txt", "w");
 	for (int i = 0; i < nextIndex; i++)
 	{
 		fprintf(fp, "%e  %e\n", array_t[i], array_E[i]);
 	}
 	fclose(fp);
+    #pragma endregion
+
+	int K = 1000;
+	double dt = Tir / K;
+
+    #pragma region IR-напряженность
+	fp = fopen("F.txt", "w");
+	for (int i = 0; i < K; i++)
+	{
+		double t = i * dt;
+		fprintf(fp, "%e  %e\n", t, F(t));
+	}
+	fclose(fp);
+    #pragma endregion
+
+    #pragma region IR-потенциал
+	fp = fopen("A.txt", "w");
+	for (int i = 0; i < K; i++)
+	{
+		double t = i * dt;
+		fprintf(fp, "%e  %e\n", t, A(t, C));
+	}
+	fclose(fp);
+    #pragma endregion
 }
 #pragma endregion
 
@@ -387,9 +409,9 @@ int work()
 
     #pragma region определение константы C
 	double C = -1;
-	for (int i = 0; i < M; i++)
+	for (int i = 0; i < 1000; i++)
 	{
-		double c = (i * 2.0 / M) - 1;
+		double c = (i * 2.0 / 1000) - 1;
 		if (abs(IntA(Tir, C) - IntA(0, C)) > abs(IntA(Tir, c) - IntA(0, c)))
 		{
 			C = c;
@@ -402,7 +424,7 @@ int work()
     #pragma endregion
 
     #pragma region построение траекторий
-	for (int i = 0; ; i++)
+	for (int i = 0;/* i < 3*/; i++)
 	{
 		double to1 = fi + (i * M_PI);
 		if (to1 > Tir)
@@ -410,12 +432,12 @@ int work()
 
 		printf("\n\nstart t1 = %d * PI\n", i);
 		printf("------------------\n", i);
-		if (trajectories(k, C, to1) == 0)
+		if (trajectories(to1, C) == 0)
 		{
 			printf("nothing\n");
 		}
 	}
-	writeFILE();
+	writeFILE(C);
 	printf("\n\n");
 	return 0;
     #pragma endregion
